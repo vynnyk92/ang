28775d01020bf21d3ca9fe3cff1b3c92b57d87af
Pylyp Vynnykov <Pylyp_Vynnykov@epam.com>
2019-08-27 11:41:44 +0300

    test

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app-root',
-  template: `<h1>Hello</h1>
+  template: `<h1>Hello test</h1>
   <img src="../assets/images/ng-nl.png" />
   `
 })
